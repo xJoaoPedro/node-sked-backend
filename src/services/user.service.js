39c07233ca0d6139d8b125b83227c48037af44cd
xJoaoPedro <pedjoness28@gmail.com>
2026-03-10 02:24:24 -0300

    correção de bug com prismaClient

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,8 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import pkg from "@prisma/client";
 import bcrypt from "bcrypt";
 
+const { PrismaClient } = pkg 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
